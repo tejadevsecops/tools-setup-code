5b48b30a95505395ff11a3c2128c3e069bc60edc
Teja <awstheja727@gmail.com>
2025-01-03 07:48:41 +0530

    Tools setup code

---
+++ misc/vault_secrets/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d80"
+    key    = "vault-secrets/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
+
+provider "vault" {
+  address         = "http://vault-internal.rdevopsb81.online:8200"
+  token           = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
+
+resource "vault_mount" "roboshop-dev" {
+  path        = "roboshop-dev"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "RoboShop Dev Secrets"
+}
+
+resource "vault_generic_secret" "roboshop-dev" {
+  path = "${vault_mount.roboshop-dev.path}/frontend"
+
+  data_json = <<EOT
+{
+  "foo":   "bar",
+  "pizza": "cheese"
+}
+EOT
+}
