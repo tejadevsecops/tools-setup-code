585bc9a44d5deb95c99e2c6178606c75126557f8
Teja <awstheja727@gmail.com>
2025-01-06 16:26:30 +0530

    Tools setup code - IAM role

---
+++ module-ec2/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
